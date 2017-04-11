22933d5ff9b3b8a0727eaeb1a9a9d7a3f24d5b3c
Evgeny N <Evgeny@macbook-air-evgeny.local>
2017-04-11 13:08:12 -0400

    Initial Commit

---
+++ QR CS50xMiami/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QR CS50xMiami
+//
+//  Created by Evgeny N on 4/11/17.
+//  Copyright © 2017 JackRus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
